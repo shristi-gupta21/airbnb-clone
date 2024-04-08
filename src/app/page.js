87d4b0397058e2eaff5fcc39a4797e2b36860cde
Shristi Gupta <gshristi40@gmail.com>
2024-04-08 22:48:34 +0530

    added header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import { Header } from "./components/Header";
+
 export default function Home() {
-  return <main>Started with next.js</main>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
